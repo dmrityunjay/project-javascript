0e364c5159e57d1af6039396417811e2cd082913
dmrityunjay <dmrityunjay258@gmail.com>
2026-09-07 20:44:01 +0530

    ready to deploy

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+//GET
+app.get('/',(req,res) =>{
+    res.send("node.js server is running");
+});
+
+//GET
+app.get("/getstudents",(req,res) => {
+    res.send("get all student");
+});
+
+//POST
+app.post("/addstudents",(req,res) => {
+    res.send("student added");
+});
+
+//PUT
+app.put("/updatestudents",(req,res) => {
+    res.send("student completely added");
+});
+
+//patch
+app.patch("/partialstudents", (req,res) => {
+    res.send("student partially updated");
+});
+
+//DELETE
+app.delete("/deletestudents",(req,res) => {
+    res.send("student delete");
+});
+
+//START SERVER
+app.listen(8800,() =>{
+    console.log("server running on port 8800");
+});
